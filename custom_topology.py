08c8b287b8b99f58a20c75306b14fed1721c33ad
pgupta8825 <57871807+pgupta8825@users.noreply.github.com>
2023-10-29 01:58:42 +0530

    custom_topology1.py

--- custom_topology.py
+++ custom_topology.py
@@ -4,28 +4,28 @@ from mininet.topo import Topo
 
 class CustomTopology(Topo):
     def __init__(self):
-        Topo.__init__(self):
+        Topo.__init__(self)
         # Add 3 switches to the topology
         switch1 = self.addSwitch('s1')
         switch2 = self.addSwitch('s2')
-        switch3 = self.addSwitch('s3')
 
         # Add 4 hosts to the topology
         host1 = self.addHost('h1')
         host2 = self.addHost('h2')
         host3 = self.addHost('h3')
         host4 = self.addHost('h4')
+        host5 = self.addHost('h5')
 
         # Connect hosts to switches
         self.addLink(host1, switch1)
         self.addLink(host2, switch1)
-        self.addLink(host3, switch2)
-        self.addLink(host4, switch3)
+        self.addLink(host3, switch1)
+        self.addLink(host4, switch2)
+        self.addLink(host5, switch2)
 
         # Connect switches together
         self.addLink(switch1, switch2)
-        self.addLink(switch2, switch3)
 
 # Create Mininet network with the custom topology
-topos = {'first':(lambda:first())}
+topos = {'first':(lambda:CustomTopology())}
 
